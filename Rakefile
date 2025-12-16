35368a0410b9d17f4dbd5acc493f714eb7e96e6c
Matt Michnal <mattm3646@gmail.com>
2025-12-15 21:54:53 -0700

    adding docs for the api calls in the usgs gem

--- Rakefile
+++ Rakefile
@@ -2,6 +2,8 @@
 
 require "bundler/gem_tasks"
 require "rake/testtask"
+require "rubocop/rake_task"
+require "yard"
 
 Rake::TestTask.new(:test) do |t|
   t.libs << "test"
@@ -9,8 +11,14 @@ Rake::TestTask.new(:test) do |t|
   t.test_files = FileList["test/**/test_*.rb"]
 end
 
-require "rubocop/rake_task"
-
 RuboCop::RakeTask.new
 
 task default: %i[test rubocop]
+
+YARD::Rake::YardocTask.new do |t|
+  t.options = ["--output-dir", "docs"]
+end
+
+task :publish_docs do
+  sh "git subtree push --prefix docs origin gh-pages"
+end
--- lib/usgs/models/statistic.rb
+++ lib/usgs/models/statistic.rb
@@ -2,6 +2,52 @@
 
 module Usgs
   module Models
+    # Represents a USGS statistic record
+    #
+    # @!attribute [rw] site_no
+    #   @return [String] USGS site number
+    # @!attribute [rw] parameter_cd
+    #   @return [String] Parameter code
+    # @!attribute [rw] month_nu
+    #   @return [Float, nil] Month number
+    # @!attribute [rw] day_nu
+    #   @return [Float, nil] Day number
+    # @!attribute [rw] begin_yr
+    #   @return [Float, nil] Beginning year
+    # @!attribute [rw] end_yr
+    #   @return [Float, nil] Ending year
+    # @!attribute [rw] count_nu
+    #   @return [Float, nil] Count of values
+    # @!attribute [rw] mean_va
+    #   @return [Float, nil] Mean value
+    # @!attribute [rw] max_va
+    #   @return [Float, nil] Maximum value
+    # @!attribute [rw] max_va_yr
+    #   @return [Float, nil] Year of maximum value
+    # @!attribute [rw] min_va
+    #   @return [Float, nil] Minimum value
+    # @!attribute [rw] min_va_yr
+    #   @return [Float, nil] Year of minimum value
+    # @!attribute [rw] p05_va
+    #   @return [Float, nil] 5th percentile value
+    # @!attribute [rw] p10_va
+    #   @return [Float, nil] 10th percentile value
+    # @!attribute [rw] p20_va
+    #   @return [Float, nil] 20th percentile value
+    # @!attribute [rw] p25_va
+    #   @return [Float, nil] 25th percentile value
+    # @!attribute [rw] p50_va
+    #   @return [Float, nil] 50th percentile (median) value
+    # @!attribute [rw] p75_va
+    #   @return [Float, nil] 75th percentile value
+    # @!attribute [rw] p80_va
+    #   @return [Float, nil] 80th percentile value
+    # @!attribute [rw] p90_va
+    #   @return [Float, nil] 90th percentile value
+    # @!attribute [rw] p95_va
+    #   @return [Float, nil] 95th percentile value
+    # @!attribute [rw] metadata
+    #   @return [Hash] Additional metadata
     class Statistic
       ATTRIBUTES = %i[
         site_no
@@ -28,16 +74,14 @@ module Usgs
         metadata
       ].freeze
 
-      attr_accessor(*ATTRIBUTES)
+      attr_accessor(*ATTRIBUTES) # :nodoc:
 
       def initialize(data = {})
         data[:metadata] ||= {}
         attrs = data.is_a?(Hash) ? data : {}
-
         ATTRIBUTES.each do |attr|
           value = attrs[attr]
 
-          # Convert all the _va fields (values) from string to float
           if value.is_a?(String) && attr.to_s.end_with?("_va", "_yr", "_nu")
             stripped = value.strip
             value = stripped.empty? || stripped == "." ? nil : stripped.to_f
